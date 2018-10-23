b6837fd363ad8c8912f2ef4d92e5ce3f969ce22f
Alexander Grooff <alexandergrooff@gmail.com>
2018-10-23 23:32:04 +0200

    Use __code__ for unify and add _reify for functions

--- unification/core.py
+++ unification/core.py
@@ -1,7 +1,6 @@
 import ast
 from functools import partial
-from types import FunctionType
-from inspect import signature
+from types import FunctionType, CodeType
 from collections import Iterator
 from toolz.compatibility import iteritems, map
 from toolz import assoc
@@ -13,6 +12,49 @@ from .dispatch import dispatch
 ################
 # Reificiation #
 ################
+
+def print_code_type(c):
+        print("consts", c.co_consts)
+        print("names", c.co_names)
+        print("varnames", c.co_varnames)
+        print("name", c.co_name)
+        print("freevars", c.co_freevars)
+        print("cellvars", c.co_cellvars)
+
+@dispatch(FunctionType, dict)
+def _reify(f, s):
+    # Since we cannot modify the bytecode of a function, we'll have to create
+    # a new function based on the bytecode from the original function.
+    # The constants used in the function are located in f.__code__.co_consts
+    # See: https://stackoverflow.com/questions/33348067/modifying-python-bytecode
+    code = f.__code__
+    if code.co_freevars:
+        return f
+    reified_constants = reify(code.co_consts, s)
+    print("Reifying function {}")
+    print_code_type(code)
+    new_byte_code = CodeType(
+        code.co_argcount,
+        code.co_kwonlyargcount,
+        code.co_nlocals,
+        code.co_stacksize,
+        code.co_flags,
+        code.co_code,
+        reified_constants,
+        code.co_names,
+        code.co_varnames,
+        code.co_filename,
+        code.co_name,
+        code.co_firstlineno,
+        code.co_lnotab,   # In general, You should adjust this
+        code.co_freevars,
+        code.co_cellvars
+    )
+    print_code_type(new_byte_code)
+
+    new_f = lambda: 123
+    new_f.__code__ = new_byte_code
+    return new_f
 
 @dispatch(Iterator, dict)
 def _reify(t, s):
@@ -83,7 +125,7 @@ def reify(e, s):
 
 @dispatch(FunctionType, FunctionType, dict)
 def _unify(u, v, s):
-    if signature(u) != signature(v):
+    if u.__code__ != v.__code__:
         return False
     return s
 
